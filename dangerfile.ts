dbfe9fa10e96b1bf60c5c5531982179fec8f7dc4
RyuNen344 <s1100633@outlook.com>
2023-01-01 06:11:33 +0900

    fix import

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,8 @@
+// @ts-ignore
+import coverage from "danger-plugin-coverage";
 import yarn from "danger-plugin-yarn";
 import jest from "danger-plugin-jest";
 
-// @ts-ignore
-const coverage = require("danger-plugin-coverage")
 const reporter = require("danger-plugin-lint-report")
 
 Promise.all([
