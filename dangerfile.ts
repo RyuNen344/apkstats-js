7620df7535edad5694178c629ba5ca4b0132010b
RyuNen344 <s1100633@outlook.com>
2023-01-01 06:03:46 +0900

    add lint check

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,5 +1,7 @@
 // @ts-ignore
-import coverage from 'danger-plugin-coverage';
+import coverage from "danger-plugin-coverage";
+// @ts-ignore
+import reporter from 'danger-plugin-lint-report';
 import yarn from "danger-plugin-yarn";
 import jest from "danger-plugin-jest";
 import {DangerDSLType} from "danger/distribution/dsl/DangerDSL";
@@ -13,5 +15,10 @@ schedule(async () => {
   }
   await yarn();
   await jest();
+  await reporter.scan({
+    fileMask: "**/lint-results.xml",
+    reportSeverity: true,
+    requireLineModification: true,
+  });
   await coverage();
 });
