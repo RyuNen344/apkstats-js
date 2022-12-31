7ccafce5b8ad4436a67c00108a6d49ac5edca8ea
RyuNen344 <s1100633@outlook.com>
2023-01-01 06:07:31 +0900

    update danger file

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,21 +4,17 @@ import coverage from "danger-plugin-coverage";
 import reporter from 'danger-plugin-lint-report';
 import yarn from "danger-plugin-yarn";
 import jest from "danger-plugin-jest";
-import {DangerDSLType} from "danger/distribution/dsl/DangerDSL";
 import {schedule} from "danger";
 
-declare const danger: DangerDSLType
-
-schedule(async () => {
-  if (!danger.github) {
-    return
-  }
-  await yarn();
-  await jest();
-  await reporter.scan({
-    fileMask: "**/lint-results.xml",
-    reportSeverity: true,
-    requireLineModification: true,
-  });
-  await coverage();
-});
+schedule(
+  Promise.all([
+    yarn(),
+    jest(),
+    reporter.scan({
+      fileMask: "**/lint-results.xml",
+      reportSeverity: true,
+      requireLineModification: true,
+    }),
+    coverage()
+  ])
+);
