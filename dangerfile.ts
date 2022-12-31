3d07da69d24a90562ca031e6d2c9540d8c972de9
RyuNen344 <s1100633@outlook.com>
2023-01-01 06:10:02 +0900

    update danger file

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,20 +1,17 @@
-// @ts-ignore
-import coverage from "danger-plugin-coverage";
-// @ts-ignore
-import reporter from 'danger-plugin-lint-report';
 import yarn from "danger-plugin-yarn";
 import jest from "danger-plugin-jest";
-import {schedule} from "danger";
 
-schedule(
-  Promise.all([
-    yarn(),
-    jest(),
-    reporter.scan({
-      fileMask: "**/lint-results.xml",
-      reportSeverity: true,
-      requireLineModification: true,
-    }),
-    coverage()
-  ])
-);
+// @ts-ignore
+const coverage = require("danger-plugin-coverage")
+const reporter = require("danger-plugin-lint-report")
+
+Promise.all([
+  yarn(),
+  jest(),
+  reporter.scan({
+    fileMask: "**/lint-results.xml",
+    reportSeverity: true,
+    requireLineModification: true,
+  }),
+  coverage()
+]).then(console.log);
