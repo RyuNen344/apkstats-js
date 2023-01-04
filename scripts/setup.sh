1a69f8e710fa9ae3d5a223bcc48d2195bd040ac5
RyuNen344 <s1100633@outlook.com>
2023-01-04 22:41:31 +0900

    create setup shell

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+. "$(git rev-parse --show-toplevel)/scripts/utilities"
+
+go_to_repo_root
+
+run sh "scripts/doctor.sh"
+run sh "scripts/prepare-fixture-apk.sh"
+run sh "scripts/prepare-fixture-output.sh"
