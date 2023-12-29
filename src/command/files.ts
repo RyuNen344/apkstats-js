99b8e8818f70f46b774f98ca9e234da1a30d1e3b
RyuNen344 <s1100633@outlook.com>
2023-12-29 23:59:46 +0900

    chore: update files if

--- src/command/files.ts
+++ src/command/files.ts
@@ -1,5 +1,7 @@
+import { FilesListOption } from "@/types/FilesListOption";
+
 export default interface Files {
-  list(): [string];
+  list(option?: FilesListOption[]): string[];
 
   cat(path: string): [string];
 }
